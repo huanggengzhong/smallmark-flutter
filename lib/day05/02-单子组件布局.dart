91f2f55ee1e58900fec6051c9886e4d5e4221627
huanggengzhong <915835528@qq.com>
2021-01-11 23:56:47 +0800

    ss

--- lib/day05/02-单子组件布局.dart
+++ lib/day05/02-单子组件布局.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(MyApp());
+main()=>  runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner:false,
       home: HomePage(),
     );
   }
@@ -16,16 +16,16 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('布局组件'),
+      appBar:AppBar(
+        title:Text('布局组件') ,
       ),
 
 //      body:AlignDemo()
 //        body:CenterDemo()
       body: HYHomeContent(),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () => print("点击了244"),
+      floatingActionButton:FloatingActionButton(
+        child:Icon(Icons.add) ,
+        onPressed:()=>print("点击了") ,
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
@@ -39,6 +39,7 @@ class HYHomeContent extends StatefulWidget {
 }
 
 class _HYHomeContentState extends State<HYHomeContent> {
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -52,20 +53,19 @@ class _HYHomeContentState extends State<HYHomeContent> {
 //      transform: Matrix4.rotationZ(50),
       decoration: BoxDecoration(
           color: Colors.red,
-          border: Border.all(width: 5, color: Colors.purple),
+          border: Border.all(
+              width: 5,
+              color: Colors.purple
+          ),
 //        borderRadius: BorderRadius.circular(100),
           boxShadow: [
-            BoxShadow(
-                color: Colors.orange,
-                offset: Offset(10, 10),
-                spreadRadius: 5,
-                blurRadius: 10),
-            BoxShadow(
-                color: Colors.blue,
-                offset: Offset(-10, 10),
-                spreadRadius: 5,
-                blurRadius: 10),
-          ]),
+            BoxShadow(color: Colors.orange, offset: Offset(10, 10), spreadRadius: 5, blurRadius: 10),
+            BoxShadow(color: Colors.blue, offset: Offset(-10, 10), spreadRadius: 5, blurRadius: 10),
+          ]
+      ),
     );
   }
 }
+
+
+
