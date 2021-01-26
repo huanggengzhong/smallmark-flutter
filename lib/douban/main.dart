3cf224ce8413cfb5ffe82f93b3fbacc01b720755
huanggengzhong <915835528@qq.com>
2021-01-26 13:32:55 +0800

    feat:完成评分

--- lib/douban/main.dart
+++ lib/douban/main.dart
@@ -20,8 +20,8 @@ class HomePage extends StatelessWidget {
       ),
       body: Center(
         child: StarRating(
-          rating:7,
-          count: 6,
+          rating: 8,
+//          count:6
         ), //测试参数传递
       ),
     );
@@ -30,18 +30,19 @@ class HomePage extends StatelessWidget {
 
 class StarRating extends StatefulWidget {
   final double rating; //多少分
-  final double maxRating; //满分是多少分
-  final int count; //数量
-  final double size; //大小
-  final Color unSelectedColor; //未选中颜色
-  final Color selectedColor; //选中颜色
+  final double maxRating; //满分是多少分,默认10分
+  final int count; //数量,默认5个
+  final double size; //图标大小,默认30
+  final Color unSelectedColor; //未选中颜色,默认灰色
+  final Color selectedColor; //选中颜色,默认红色
 
-  StarRating({@required this.rating, //必选参数
-    this.maxRating = 10, //这里开始是可选参数,并给默认值
-    this.count = 5,
-    this.size = 30,
-    this.unSelectedColor = const Color(0xffbbbbbb), //默认rgb颜色,要用常量声明
-    this.selectedColor = Colors.red});
+  StarRating(
+      {@required this.rating, //必选参数
+      this.maxRating = 10, //这里开始是可选参数,并给默认值
+      this.count = 5,
+      this.size = 30,
+      this.unSelectedColor = const Color(0xffbbbbbb), //默认rgb颜色,要用常量声明
+      this.selectedColor = Colors.red});
 
   @override
   _StarRatingState createState() => _StarRatingState();
@@ -71,19 +72,26 @@ class _StarRatingState extends State<StarRating> {
 
 //    2.创建满填充的star
 
-    double oneValue=widget.maxRating / widget.count;//一个星占多少
-    int entireCount=(widget.rating / oneValue).floor();//多少个满
-
+    double oneValue = widget.maxRating / widget.count; //一个星占多少分
+    int entireCount = (widget.rating / oneValue).floor(); //计算出多少个满
 
 //    添加满星
-    for(var i=0;i<entireCount;i++){
+    for (var i = 0; i < entireCount; i++) {
       stars.add(star);
     }
-//    //3.没有满填充的star,用到裁剪组件ClipRect
+//    //3.没有满填充的star,用到裁剪组件ClipRect 3.5-3=0.5*widget.size
+      double leftWidth=((widget.rating/oneValue)-entireCount)*widget.size;
+    final halfStar=ClipRect(
+      clipper:StarRect(leftWidth) ,
+      child: star,
+    );
+
+    stars.add(ClipRect(
+      child: halfStar,
+    ));
 
 //    将最终结果返回
     return stars;
-
   }
 
   @override
@@ -96,4 +104,21 @@ class _StarRatingState extends State<StarRating> {
       ),
     ]);
   }
+}
+
+//矩形裁剪
+
+class StarRect extends CustomClipper<Rect>{//传入Rect矩形作为泛型
+  double width;
+  StarRect(this.width);
+  @override
+  Rect getClip(Size size) {
+    return Rect.fromLTRB(0, 0, width, size.height);//裁剪宽度,高度不变
+  }
+
+  bool shouldReclip(CustomClipper oldClipper){//要不要重新裁剪
+    return oldClipper!=this.width;
+  }
+
+
 }
