5c343f12008191a4720780c1c084c936340ef40c
huanggengzhong <915835528@qq.com>
2021-01-30 23:14:58 +0800

    feat:接口联调

--- lib/douban/pages/home/HomeContent.dart
+++ lib/douban/pages/home/HomeContent.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutterapp3/service/http_request.dart';
+
+//import ' service/http_request.dart';
 class HomeContent extends StatefulWidget {
   @override
   _HomeContentState createState() => _HomeContentState();
@@ -6,9 +9,26 @@ class HomeContent extends StatefulWidget {
 
 class _HomeContentState extends State<HomeContent> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //这里发送网络请求
+    HttpRequest.request('/get', params: {"name": 'why'}).then((res) {
+      print('get请求打印结果:$res');
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("首页"),
+      child: ListView.builder(
+        itemCount: 30,
+        itemBuilder: (ctx, index) {
+          return ListTile(
+            title: Text('item$index'),
+          );
+        },
+      ),
     );
   }
 }
