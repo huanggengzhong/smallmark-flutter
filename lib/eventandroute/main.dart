cbd5c45c1458293e594e25a146b1f05a09f14887
huanggengzhong <915835528@qq.com>
2021-02-06 17:06:16 +0800

    pref:事件开始

--- lib/eventandroute/main.dart
+++ lib/eventandroute/main.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
-main(){
+
+main() {
   return runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("text",textDirection:TextDirection.ltr),
+    return MaterialApp(
+      home:Scaffold(
+        appBar: AppBar(
+          title: Text("事件监听"),
+        ),
+        body: Center(
+//          Listener事件的使用
+          child: Listener(
+              onPointerDown: (event) {
+                print("事件开始:$event");
+              },
+              child: Container(
+                width: 200,
+                height: 200,
+                color: Colors.red,
+              )),
+        ),
+      )
     );
   }
 }
