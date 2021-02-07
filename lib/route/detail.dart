113b4dd7f82f38aaa17d4e4328019e1f1f08bccb
huanggengzhong <915835528@qq.com>
2021-02-07 11:26:28 +0800

    feat:普通跳转完成

--- lib/route/detail.dart
+++ lib/route/detail.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+
 class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+//    回到首页
+    void _backToHome(BuildContext context) {
+      Navigator.of(context).pop();
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text("详情页"),
       ),
-      body: Container(
-        child: Text("我是详情页"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("详情页"),
+            RaisedButton(
+                child: Text("回到首页"), onPressed: () => _backToHome(context))
+          ],
+        ),
       ),
     );
   }
