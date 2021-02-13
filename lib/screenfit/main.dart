65fec505d2f9f046379245d43120279a8ae6e8d9
huanggengzhong <915835528@qq.com>
2021-02-13 12:10:36 +0800

    feat:完成屏幕视频

--- lib/screenfit/main.dart
+++ lib/screenfit/main.dart
@@ -1,140 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'shared/size_fit.dart';
+import './extension/double_extension.dart';
+import './extension/int_extension.dart';
 
 void main() => runApp(MyApp());
+
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //Set the fit size (fill in the screen size of the device in the design) If the design is based on the size of the iPhone6 ​​(iPhone6 ​​750*1334)
-    return ScreenUtilInit(
-      designSize: Size(750, 1334),
-      allowFontScaling: false,
-      builder: () => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter_ScreenUtil',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: HomePage(title: 'FlutterScreenUtil Demo'),
-      ),
+    HYSizeFit.initialize();
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blue, splashColor: Colors.transparent),
+      home: HYHomePage(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
+class HYHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    printScreenInformation();
+
+    // 2.手机屏幕的大小(逻辑分辨率)
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    final statusHeight = MediaQuery.of(context).padding.top;
+    print("屏幕宽高: $width * $height");
+    print("状态栏的高度: $statusHeight");
+    print(HYSizeFit.screenWidth);
+
+    print(200.px);
+    print(200.0.px);
+    print(400.rpx);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("首页"),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.all(ScreenUtil().setWidth(10)),
-                  width: 180.w,
-                  height: 200.h,
-                  color: Colors.red,
-                  child: Text(
-                    '我的实际宽度:${0.5.sw}dp \n'
-                        '我的实际高度:${ScreenUtil().setHeight(200)}dp',
-                    style: TextStyle(color: Colors.white, fontSize: ScreenUtil().setSp(12)),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(ScreenUtil().setWidth(10)),
-                  width: ScreenUtil().setWidth(180),
-                  height: ScreenUtil().setHeight(200),
-                  color: Colors.blue,
-                  child: Text(
-                      '我的设计稿宽度: 180dp \n'
-                          '我的设计稿高度: 200dp',
-                      style: TextStyle(color: Colors.white, fontSize: ScreenUtil().setSp(12))),
-                ),
-              ],
-            ),
-            Container(
-              padding: EdgeInsets.all(ScreenUtil().setWidth(10)),
-              width: 100.r,
-              height: 100.r,
-              color: Colors.green,
-              child: Text(
-                '我是正方形,边长是100',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: ScreenUtil().setSp(12),
-                ),
-              ),
-            ),
-            Text('设备宽度:${ScreenUtil().screenWidth}dp'),
-            Text('设备高度:${ScreenUtil().screenHeight}dp'),
-            Text('设备的像素密度:${ScreenUtil().pixelRatio}'),
-            Text('底部安全区距离:${ScreenUtil().bottomBarHeight}dp'),
-            Text('状态栏高度:${ScreenUtil().statusBarHeight}dp'),
-            Text(
-              '实际宽度与设计稿的比例:${ScreenUtil().scaleWidth}',
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              '实际高度与设计稿的比例:${ScreenUtil().scaleHeight}',
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 50.h,
-            ),
-            Text('系统的字体缩放比例:${ScreenUtil().textScaleFactor}'),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  '我的文字大小在设计稿上是16dp，不会随着系统的文字缩放比例变化',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16.sp,
-                  ),
-                ),
-                Text(
-                  '我的文字大小在设计稿上是16dp，会随着系统的文字缩放比例变化',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16.ssp,
-                  ),
-                ),
-              ],
-            )
-          ],
+      body: Center(
+        child: Container(
+          width: 200.px,
+          height: 200.px,
+          color: Colors.red,
+          alignment: Alignment.center,
+          child: Text("Hello World", style: TextStyle(fontSize: 20.px),),
         ),
       ),
     );
-  }
-
-  void printScreenInformation() {
-    print('设备宽度:${1.sw}dp');
-    print('设备高度:${1.sh}dp');
-    print('设备的像素密度:${ScreenUtil().pixelRatio}');
-    print('底部安全区距离:${ScreenUtil().bottomBarHeight}dp');
-    print('状态栏高度:${ScreenUtil().statusBarHeight}dp');
-    print('实际宽度的dp与设计稿px的比例:${ScreenUtil().scaleWidth}');
-    print('实际高度的dp与设计稿px的比例:${ScreenUtil().scaleHeight}');
-    print('宽度和字体相对于设计稿放大的比例:${ScreenUtil().scaleWidth * ScreenUtil().pixelRatio}');
-    print('高度相对于设计稿放大的比例:${ScreenUtil().scaleHeight * ScreenUtil().pixelRatio}');
-    print('系统的字体缩放比例:${ScreenUtil().textScaleFactor}');
-    print('屏幕宽度的0.5:${0.5.sw}dp');
-    print('屏幕高度的0.5:${0.5.sh}dp');
   }
 }
