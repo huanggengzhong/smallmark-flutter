0fb4a27e0efaf016c2e71bb3ef499e3c4f533ceb
huanggengzhong <915835528@qq.com>
2021-02-19 18:54:28 +0800

    feat:增加字体

---
+++ lib/laomeng/01-font/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Demo",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,splashColor:Colors.transparent,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("首页"),
+      ),
+      body: Center(
+
+        child: Text("字由字体工具",style: TextStyle(fontFamily:'ksw',color: Colors.black,fontSize: 30),),
+      ),
+    );
+  }
+}
+
