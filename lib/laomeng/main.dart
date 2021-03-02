91ff937264848b640c6df3a74f81200664670454
huanggengzhong <915835528@qq.com>
2021-03-02 23:57:12 +0800

    feat:睡觉

--- lib/laomeng/main.dart
+++ lib/laomeng/main.dart
@@ -24,15 +24,8 @@ class HomePage extends StatelessWidget {
       ),
       body: Container(
 //        发现TextRich没有用,用下面语法有用
-        child: TextField(
-          decoration: InputDecoration(
-            hintText: "请输入姓名",
-            labelText:"姓名",
-            labelStyle: TextStyle(color: Colors.red),
-            helperText: "姓名长度为6-10个字母",
-            helperMaxLines: 1,
-            helperStyle: TextStyle(color: Colors.blue)
-          ),
+        child: RaisedButton(
+          child: Text("raiseButton"),
         ),
       ),
     );
