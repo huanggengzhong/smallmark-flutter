b542584d3cc8659c1c34f71be8de1e7d699906f9
huanggengzhong <915835528@qq.com>
2021-03-15 09:35:32 +0800

    文字过渡

--- lib/laomeng/main.dart
+++ lib/laomeng/main.dart
@@ -18,15 +18,23 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+      RenderBox box = context.findRenderObject();
+      final Shader linearGradient = LinearGradient(
+        colors: <Color>[Colors.red, Colors.blue],
+      ).createShader(
+          Rect.fromLTWH(0.0, 0.0, box?.size?.width, box?.size?.height));
     return Scaffold(
       appBar: AppBar(
         title: Text("首页"),
       ),
       body: Container(
-//        发现TextRich没有用,用下面语法有用
-        child: RaisedButton(
-          child: Text("raiseButton"),
-        ),
+        child: Text(
+          '老孟，专注分享Flutter技术和应用实战',
+          style: new TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+              foreground: Paint()..shader = linearGradient),
+        )
       ),
     );
   }
